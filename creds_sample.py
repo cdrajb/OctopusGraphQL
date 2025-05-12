c94582d48eb3bbced948e9abeb6250c1f89b3474
cdrajb <84205184+cdrajb@users.noreply.github.com>
2025-05-12 19:20:34 +0100

    Create creds_sample.py

---
+++ creds_sample.py
@@ -0,0 +1,3 @@
+apikey="sk_live_...." # Your Octopus API Key
+accountNumber="A-A....." # Your Octopus Account Number
+propertyId='nnnnnn'	# Your property id (if you don't know this, run octo_account.py to find it)
