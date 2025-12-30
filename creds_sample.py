1cc17859e56c871be3c7ae92d374df847b0be5a3
cdrajb <84205184+cdrajb@users.noreply.github.com>
2025-12-30 21:33:54 +0000

    Update creds_sample.py

--- creds_sample.py
+++ creds_sample.py
@@ -1,3 +1,4 @@
 apikey="sk_live_...." # Your Octopus API Key
 accountNumber="A-A....." # Your Octopus Account Number
 propertyId='nnnnnn'	# Your property id (if you don't know this, run octo_account.py to find it)
+mpan='nnnnnnnnn' # your electricity meter's MPAN (also reported by octo_account.py)
